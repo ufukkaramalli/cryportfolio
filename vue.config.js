3857c8a7abc7c38138460c046b3141c07b98b4c6
Ufuk Karamalli <ufukkaramalli@gmail.com>
2022-04-25 02:51:10 +0300

    i18n added

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,14 @@ module.exports = {
   pwa: {
     name: 'Binance',
     manifestCrossorigin: 'anonymous'
+  },
+  pluginOptions: {
+    i18n: {
+      locale: 'ja',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+      enableLegacy: false
+    }
   }
 }
