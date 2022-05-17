53f71decae723d5faba85be9ee8c20539375dbad
Ufuk Karamalli <ufukkaramalli@gmail.com>
2022-05-17 22:22:08 +0300

    updated

--- src/utils/calculate.js
+++ src/utils/calculate.js
@@ -1,15 +1,15 @@
-export function calculateUSDT(marketData,payload){
-    if(payload.asset === 'USDT' || payload.asset === 'USD'){
+module.exports = (marketData, payload) => {
+    if (payload.asset === 'USDT' || payload.asset === 'USD') {
         return payload.value
-    }else{
-        let name = payload.asset+"USDT"
+    } else {
+        let name = payload.asset + "USDT"
         let price = null
         marketData.find(element => {
-            if(name === element.symbol){
+            if (name === element.symbol) {
                 price = element.lastPrice
             }
         })
-        let a = parseFloat(payload.value)*parseFloat(price)
+        let a = parseFloat(payload.value) * parseFloat(price)
         return a
     }
-  }
+}
