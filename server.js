11aabc970612f7ef9429e6c070ba9b8450d6f96a
Ufuk Karamalli <ufukkaramalli@gmail.com>
2022-05-17 22:20:21 +0300

    routes added

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const dotenv = require('./server/Utils/dotenv')
 const express = require('express')
 const cors = require('cors')
-const Home = require('./server/Routes/Home')
+const BinanceRoutes = require('./server/Routes/Binance')
+const FtxRoutes = require('./server/Routes/FTX')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use('/binance',Home)
+app.use('/binance',BinanceRoutes)
+app.use('/ftx',FtxRoutes)
 
 app.listen(parseInt(5000),() => {
     console.log(`We are live on ${process.env.NODE_ENV} mode on port 5000`)
