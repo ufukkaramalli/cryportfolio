786950e3678eb470c62c287d96f19ea5453885ad
Ufuk Karamalli <ufukkaramalli@gmail.com>
2022-05-22 21:15:07 +0300

    computed property added for symbols

--- src/components/TradingView.vue
+++ src/components/TradingView.vue
@@ -1,24 +1,32 @@
 <template>
 <div class="col-12 col-md-12 g-2">
     <div class="card p-2 text-dark" >
-        <VueTradingView style="height:600px" :options="{symbol: `${symbols}`,theme: 'dark',autosize: true,allow_symbol_change: false,show_popup_button: true,popup_width:1000,popup_height:650,hide_legend:true}"/>
+        <VueTradingView style="height:600px" :options="{symbol:`${getSymbols}`,theme: 'dark',autosize: true,allow_symbol_change: false,show_popup_button: true,popup_width:1000,popup_height:650,hide_legend:true}"/>
     </div>
 </div>
 </template>
-
 <script>
 import VueTradingView from 'vue-trading-view';
 export default {
   name: 'TradingView',
-  props: {
-    symb: String
-  },
+  props: ['symb'],
+  data: () => ({
+    symbol: null
+  }),
   components: {
     VueTradingView,
   },
+  created(){
+    this.getSymbols = this.symb
+  },
   computed:{
-      symbols: function(){
-          return this.symb
+      getSymbols:{
+        get(){
+          return this.symbol
+        },
+        set(val){
+          this.symbol = val
+        }
       }
   }
 }
