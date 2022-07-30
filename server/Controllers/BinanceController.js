897d4514663434b4507b89036bb8ac44bf692274
Ufuk Karamalli <ufukkaramalli@gmail.com>
2022-07-31 01:14:16 +0300

    unlisting changes for luna and luna classic

--- server/Controllers/BinanceController.js
+++ server/Controllers/BinanceController.js
@@ -16,14 +16,17 @@ module.exports = {
             let { data } = await axios.get("https://api.binance.com/api/v3/ticker/24hr")
             let { balances } = await BinanceClient.getAccountInformation()
             let e = balances.filter(element => {
-                if(element.free > 0 || element.locked > 0){
-                    element.where="BINANCE"
-                    element.total=parseFloat(element.free)+parseFloat(element.locked)
-                    element.usdt = calculateUSDT(data,{value:element.total,asset:element.asset})
-                    delete element.free
-                    delete element.locked
-                    binance_grandTotal_usdt = binance_grandTotal_usdt + parseFloat(element.usdt)
-                    return element
+                console.log(element)
+                if(element.asset !== 'LUNC' && element.asset !== 'LUNA'){
+                    if(element.free > 0 || element.locked > 0){
+                        element.where="BINANCE"
+                        element.total=parseFloat(element.free)+parseFloat(element.locked)
+                        element.usdt = calculateUSDT(data,{value:element.total,asset:element.asset})
+                        delete element.free
+                        delete element.locked
+                        binance_grandTotal_usdt = binance_grandTotal_usdt + parseFloat(element.usdt)
+                        return element
+                    }
                 }
             })
             let response = {balances:e,grandtotal:binance_grandTotal_usdt}
